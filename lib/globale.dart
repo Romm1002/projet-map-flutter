1e4a7c07026eaea3cf1a8976a7d44f290047e5ba
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2023-07-20 17:05:37 +0200

    Default user (#8)

--- lib/globale.dart
+++ lib/globale.dart
@@ -2,5 +2,5 @@
 import '../model/user.dart';
 
 
-MyUser me = MyUser.empty();
+MyUser me = MyUser.defaut();
 String defaultImage = "https://tse1.mm.bing.net/th?id=OIP.zRmpjD_EOxCboGENHfjxHAHaEc&pid=Api";
