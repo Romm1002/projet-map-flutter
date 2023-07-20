df4c20e96e29570327c3f7cd18b214838d79fe57
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2023-07-20 14:18:31 +0200

    user model + global var file (#1)

---
+++ lib/globale.dart
@@ -0,0 +1,7 @@
+import '../model/user.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+enum Gender {homme,femme,transgenre,indefini}
+
+MyUser me = MyUser.empty();
+String defaultImage = "https://tse1.mm.bing.net/th?id=OIP.zRmpjD_EOxCboGENHfjxHAHaEc&pid=Api";
