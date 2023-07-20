30bb5385480c4633aa48776549023f9893d32b25
zbrwkq <Damien.chv78@gmail.com>
2023-07-20 16:15:36 +0200

    Bugfix

--- lib/globale.dart
+++ lib/globale.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+
 import '../model/user.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 enum Gender {homme,femme,transgenre,indefini}
 
 MyUser me = MyUser.empty();
+MaterialColor mainColor = Colors.lightBlue;
 String defaultImage = "https://tse1.mm.bing.net/th?id=OIP.zRmpjD_EOxCboGENHfjxHAHaEc&pid=Api";
